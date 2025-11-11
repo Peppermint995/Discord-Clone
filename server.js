c380b0e50496d422bcc68f2bb6cf2e473d045b92
Peppermint995 <uyiog_aghay195@ahschools.us>
2025-11-11 17:28:12 -0600

    Set up Express server with Socket.io support

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, { cors: { origin: '*' } });
+
+app.use(express.static('public'));
+
+let users = {};
+
+io.on('connection', socket => {
+  console.log('User connected:', socket.id);
+
+  socket.on('set username', username => {
+    users[socket.id] = username;
+    io.emit('user list', Object.values(users));
+  });
+
+  socket.on('chat message', msg => {
+    const username = users[socket.id] || 'Anonymous';
+    io.emit('chat message', { user: username, msg });
+  });
+
+  socket.on('disconnect', () => {
+    delete users[socket.id];
+    io.emit('user list', Object.values(users));
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
